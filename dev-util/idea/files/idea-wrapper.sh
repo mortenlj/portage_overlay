65dfd66796f1172229d2b920b002af187e356e8f
Morten Lied Johansen <mortenjo@ifi.uio.no>
2016-01-13 21:35:16 +0100

    Incorporate smarter idea cmdline ideas in launcher-script

---
+++ dev-util/idea/files/idea-wrapper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# check for where the latest version of IDEA is installed
+IDEA=@@IDEA_EXECUTABLE@@
+wd=`pwd`
+
+# were we given a directory?
+if [[ -d "$1" ]]; then
+#  echo "checking for things in the working dir given"
+  wd=`ls -1d "$1" | head -n1`
+fi
+
+# were we given a file?
+if [[ -f "$1" ]]; then
+#  echo "opening '$1'"
+  exec "$IDEA" "$1"
+else
+  # let's check for stuff in our working directory.
+  pushd $wd > /dev/null
+
+  # does our working dir have an .idea directory?
+  if [[ -d ".idea" ]]; then
+#    echo "opening via the .idea dir"
+    exec "$IDEA" .
+
+  # is there an IDEA project file?
+  elif [[ -f *.ipr ]]; then
+#    echo "opening via the project file"
+    exec "$IDEA" `ls -1d *.ipr | head -n1`
+
+  # Is there a pom.xml?
+  elif [[ -f pom.xml ]]; then
+#    echo "importing from pom"
+    exec "$IDEA" "pom.xml"
+
+  # can't do anything smart; just open IDEA
+  else
+#    echo 'cbf'
+    exec "$IDEA"
+  fi
+
+  popd > /dev/null
+fi
